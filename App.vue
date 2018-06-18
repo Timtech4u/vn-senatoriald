beed3045d47fa930ac535338953d573646eb2b65
Timothy Olaleke <timothyolaleke@gmail.com>
2018-06-18 13:13:04 +0100

    My codes

---
+++ App.vue
@@ -0,0 +1,55 @@
+<template>
+  <view class="container">
+    <text class="text-color-primary">Find Senatorial District</text>
+        <text-input
+        :style="{height: 40, borderColor: 'gray', borderWidth: 1}"
+        v-model="text"
+      />
+    <button
+        :on-press="senatorialDistrict(text.toLowerCase())"
+        title="Find"
+        color="#841584"
+        accessibility-label="Find my Senatorial District"
+    />
+    <text class="text-color-primary">{{message}}</text>
+    </view>
+</template>
+ 
+<style>
+.container {
+  flex: 1;
+  background-color: white;
+  align-items: center;
+  justify-content: center;
+}
+.text-color-primary {
+  color: blue;
+  font-size: 30;
+}
+</style>
+
+
+<script>
+import senDis from '.senDis.js'
+export default {
+    data() {
+      return {
+        text: '',
+        message: ''
+      }
+    },
+    methods: {
+    senatorialDistrict(userLga){
+      let result = ''
+      for (const sD in senDis) {
+          senDis[sD].forEach(element => {
+              if (userLga==element.toLowerCase()){
+                  result=sD
+              }
+          });
+      }
+      this.message = result
+    }
+  }
+}
+</script>
---
+++ senDis.js
@@ -0,0 +1,664 @@
+export default {
+    "AbiaNorth":[
+       "Umonneochi",
+       "Isukwuato",
+       "Ohafia",
+       "Arochukwu",
+       "Bende"
+    ],
+    "AbiaCentral":[
+       "Umuahia North",
+       "Umuahia South",
+       "Ikwuano",
+       "Isiala Ngwa North",
+       "Isiala Ngwa South"
+    ],
+    "AbiaSouth":[
+       "Aba North",
+       "Aba South",
+       "Ugwunagbo",
+       "Obingwa",
+       "Ukwa East",
+       "Ukwa West",
+       "Osisioma"
+    ],
+    "AdamawaNorth":[
+       "Madagali",
+       "Maiha",
+       "Michika",
+       "Mubi North",
+       "Mubi South"
+    ],
+    "AdamawaSouth":[
+       "Demsa",
+       "Ganye",
+       "Guyuk",
+       "Jada",
+       "Mayo-Belwa",
+       "Numan",
+       "Shelleng",
+       "Toungo",
+       "Lamurde"
+    ],
+    "AdamawaCentral":[
+       "Hong",
+       "Fufore",
+       "Song",
+       "Girei",
+       "Yola North",
+       "Yola South"
+    ],
+    "AkwaIbomEast":[
+       "Etinan",
+       "Ibesikpo Asutan",
+       "Ibiono Ibom",
+       "Itu",
+       "Nsit Atai",
+       "Nsit Ibom",
+       "Nsit Ubium",
+       "Uruan",
+       "Uyo"
+    ],
+    "AkwaIbomWest":[
+       "Abak",
+       "Essien Udim",
+       "Etim Ekpo",
+       "Ika",
+       "Ikono",
+       "Ikot",
+       "Ekpene",
+       "Ini",
+       "Obot Akara",
+       "Oruk Anam",
+       "Ukanafun"
+    ],
+    "AkwaIbomSouth":[
+       "Eastern Obolo",
+       "Eket",
+       "Esit Eket",
+       "Ibeno",
+       "Ikot Abasi",
+       "Mbo",
+       "Mkpat Enin",
+       "Okobo",
+       "Onna",
+       "Oron",
+       "Udung Uko",
+       "Urue Offong/Oruku"
+    ],
+    "AnambraNorth":[
+       "Onitsha North",
+       "Onitsha South",
+       "Oyi",
+       "Ogbaru",
+       "Anambra East",
+       "Anambra West",
+       "Ayemelum"
+    ],
+    "AnabraCentral":[
+       "Awka North",
+       "Awka South",
+       "Njikoka",
+       "Anaocha",
+       "Idemili Noth",
+       "Idemili South",
+       "Dunukofia"
+    ],
+    "AnambraSouth":[
+       "Ihiala",
+       "Nnewi North",
+       "Nnewi South",
+       "Orumba North",
+       "Orumba South",
+       "Aguata",
+       "Ekwusigo"
+    ],
+    "BauchiSouth":[
+       "Alkaleri",
+       "Kirfi",
+       "Bauchi",
+       "Tafawa Balewa",
+       "Bogoro",
+       "Dass",
+       "Toro"
+    ],
+    "BauchiCentral":[
+       "Ningi",
+       "Warji",
+       "Darazo",
+       "Ganjuwa",
+       "Misau",
+       "Damban"
+    ],
+    "BauchiNorth":[
+       "zaki",
+       "Gamawa",
+       "Jama'are",
+       "Itas-Gadau",
+       "Shira",
+       "Giade",
+       "Katagum"
+    ],
+    "BayelsaEast":[
+       "Brass",
+       "Nembe",
+       "Ogbia"
+    ],
+    "BayelsaCentral":[
+       "Kolokuma/Opokuma",
+       "Southern Ijaw",
+       "Yenagoa"
+    ],
+    "BAyelsaWest":[
+       "Ekeremor",
+       "Sagbama"
+    ],
+    "BenueEast":[
+       "KAtsina-Ala",
+       "Konshisha",
+       "Kwande",
+       "Logo",
+       "Ukum",
+       "Ushongo",
+       "Vandeikya"
+    ],
+    "BenueWest":[
+       "Buruku",
+       "Gboko",
+       "Guma",
+       "Gwer-East",
+       "Gwer-West",
+       "Makurdi",
+       "Tarka"
+    ],
+    "BenueSouth":[
+       "Ado",
+       "Agatu",
+       "Apa",
+       "Obi",
+       "Ogbadibo",
+       "Ohimini",
+       "Oju",
+       "Okpokwu",
+       "Utukpo"
+    ],
+    "BornoNorth":[
+       "Abadam",
+       "Gubio",
+       "Guzamala",
+       "Magumeri",
+       "Marte",
+       "Mobbar",
+       "Nganza",
+       "Moguno"
+    ],
+    "BornoCentral":[
+       "Dikwa",
+       "Jere",
+       "Kala Balge",
+       "Mafa",
+       "Metropolitan",
+       "Ngala",
+       "Konduga",
+       "Bama",
+       "Kaga"
+    ],
+    "BornoSouth":[
+       "Askira/Uba",
+       "Bayo",
+       "Biu",
+       "Chibok",
+       "Damboa",
+       "Gwoza",
+       "Hawul",
+       "Kwaya",
+       "Shani"
+    ],
+    "CrossRiverNorth":[
+       "Bekwarra",
+       "Obanliku",
+       "Obudu",
+       "Ogoja",
+       "Yala",
+       "Biase"
+    ],
+    "CrossRiverCentral":[
+       "Abi",
+       "Boki",
+       "Etung",
+       "Ikom",
+       "Obubra",
+       "Yakurr"
+    ],
+    "CrossRiverSouth":[
+       "Calabar Municipal",
+       "Calabar South",
+       "Akamkpa",
+       "Akpabuyo",
+       "Bakassi",
+       "Odukpani"
+    ],
+    "DeltaCentral":[
+       "Ethiope East",
+       "Ethiope West",
+       "Okpe",
+       "Sapele",
+       "Udu",
+       "Ughelli North",
+       "Ughelli South",
+       "Uvwie"
+    ],
+    "DeltaNorth":[
+       "Aniocha North",
+       "Aniocha South",
+       "Ika North-East",
+       "Ika South",
+       "Ndokwa East",
+       "Ndokwa West",
+       "Oshimili North",
+       "Oshimili South",
+       "Ukwuani"
+    ],
+    "DeltaSouth":[
+       "Bomadi",
+       "Burutu",
+       "Isoko North",
+       "Isoko South",
+       "Patani",
+       "Warri North",
+       "Warri South",
+       "Warri South West"
+    ],
+    "EbonyiNorth":[
+       "Abakaliki",
+       "Ebonyi",
+       "Izzi",
+       "Ohaukwu"
+    ],
+    "EbonyiCentral":[
+       "Ezza North",
+       "Ezza South",
+       "Ikwo",
+       "Ishielu"
+    ],
+    "EbonyiSouth":[
+       "Afikpo North",
+       "Afikpo South",
+       "Ivo",
+       "Ohaozara",
+       "Onicha"
+    ],
+    "EdoCentral":[
+       "Esan Central",
+       "Esan North East",
+       "Esan South East",
+       "Esan West",
+       "Igueben"
+    ],
+    "EdoNOrth":[
+       "Akoko Edo",
+       "Etsako East",
+       "Etsako Central",
+       "Etsako West",
+       "Owan East",
+       "Owan West"
+    ],
+    "EdoSouth":[
+       "Oredo",
+       "Orhionmwon",
+       "Ovia North East",
+       "Ovia South West",
+       "Egor",
+       "Uhunmwode",
+       "Ikpoba Okha"
+    ],
+    "EkitiNorth":[
+       "Ikole",
+       "Oye",
+       "Ido/Osi",
+       "Moba",
+       "Ilejemeje"
+    ],
+    "EkitiCentral":[
+       "Ado Ekiti",
+       "Irepodun/Ifelodun",
+       "Ijero",
+       "Efon",
+       "Ekiti West"
+    ],
+    "EkitiSouth":[
+       "Ekiti SOuth West",
+       "Ikere",
+       "Emure",
+       "Ise/Orun",
+       "Gbonyin",
+       "Ekiti East"
+    ],
+    "EnuguEast":[
+       "Enugu East",
+       "Enugu North",
+       "Enugu South",
+       "Isi-Uzo",
+       "Nnkanu East",
+       "Nnkanu West"
+    ],
+    "EnuguWest":[
+       "Aniniri",
+       "Awgu",
+       "Ezeagu",
+       "Oji River",
+       "Udi"
+    ],
+    "EnuguNorth":[
+       "Igbo-Etiti",
+       "Igbo-Eze North",
+       "Igbo-Eze South",
+       "Uzo Uwani",
+       "Udenu",
+       "Nsukka"
+    ],
+    "GombeCentral":[
+       "Akko",
+       "Yamaltu/Deba"
+    ],
+    "GombeSouth":[
+       "Balanga",
+       "Billiri",
+       "Kaltungo",
+       "Shongom"
+    ],
+    "GombeNorth":[
+       "Funakaye",
+       "Nafada",
+       "Gombe",
+       "Kwami",
+       "Dukku"
+    ],
+    "ImoEast":[
+       "Aboh Mbaise",
+       "Ahiazu",
+       "Mbaise",
+       "Ezinihitte",
+       "Ikeduru",
+       "Mbaitoli",
+       "Ngor Okpala",
+       "Owerri Municipal",
+       "Owerri North",
+       "Owerri West"
+    ],
+    "ImoWest":[
+       "Ideato North",
+       "Ideato South",
+       "Isu",
+       "Njaba",
+       "Nkwere",
+       "Nwangele",
+       "Oguta",
+       "Ohaji/Egbema",
+       "Orlu",
+       "Orsu",
+       "Oru West",
+       "Oru East"
+    ],
+    "ImoNorth":[
+       "Ehime Mbano",
+       "Ihite/Uboma",
+       "Isiala Mbano",
+       "Obowo",
+       "Okigwe",
+       "Onuimo"
+    ],
+    "JigawaSouth-West":[
+       "Birnin Kudu",
+       "Buji",
+       "Dutse",
+       "Gwaram",
+       "Kiyawa",
+       "Jaun",
+       "Miga"
+    ],
+    "JigawaNorth-East":[
+       "Auyo",
+       "Birniwa",
+       "Guri",
+       "Hadejia",
+       "K/Hausa",
+       "K/Kasamma",
+       "M/Madori"
+    ],
+    "JigawaNorth-West":[
+       "Babura",
+       "Gagara",
+       "Garki",
+       "Gwiwa",
+       "Kazaure",
+       "Maigatar",
+       "Roni",
+       "Ringin",
+       "S/Tankar",
+       "Taura",
+       "Yankwashi",
+       "Gume"
+    ],
+    "KadunaNorth":[
+       "Kubau",
+       "Ikara",
+       "Makarfi",
+       "Sabon Gari",
+       "Soba",
+       "Zaria",
+       "Lere",
+       "Kudan"
+    ],
+    "KadunaCentral":[
+       "Birnin Gwari",
+       "Giwa",
+       "Igabi",
+       "Kaduna North",
+       "Kaduna South",
+       "Chikun",
+       "Kajuru"
+    ],
+    "KadunaSouth":[
+       "Jema'a",
+       "Jaba",
+       "Kaura",
+       "Zangon",
+       "Kaura",
+       "Kachia",
+       "Kagarko",
+       "Sanga"
+    ],
+    "KanoCentral":[
+       "Dala",
+       "Gwale",
+       "Dawakin",
+       "Gezawa",
+       "Tarauni",
+       "Fagge",
+       "Garum Mallam",
+       "Kano Municipal",
+       "Kumbotso",
+       "Kura",
+       "Madobi",
+       "Minjibir",
+       "Nassarawa",
+       "Ungogo",
+       "Warawa"
+    ],
+    "KanoNorth":[
+       "Bichi",
+       "Shanono",
+       "Bagwai",
+       "Danbatta",
+       "Makoda",
+       "Gabasawa",
+       "Gwarzo",
+       "Kabo",
+       "Rimi Gado",
+       "Tofa",
+       "Tsanyawa",
+       "Kunchi",
+       "Karaye"
+    ],
+    "KanoSouth":[
+       "Albasu",
+       "Bebeji",
+       "Bunkure",
+       "Doguwa",
+       "Gaya",
+       "Kiru",
+       "Rano",
+       "Takai",
+       "Ajingi",
+       "Rogo",
+       "Kibiya",
+       "Tudun Wada",
+       "Garko",
+       "Wudil",
+       "Sumaila"
+    ],
+    "KatsinaNorth":[
+       "Daura",
+       "Zango",
+       "Maiadua",
+       "Mashi",
+       "Mani",
+       "Ingawa",
+       "Bindawa",
+       "Sandamu",
+       "Dutsu",
+       "Kankia",
+       "Kusada",
+       "Baure"
+    ],
+    "KatsinaSouth":[
+       "Funtua",
+       "Faskari",
+       "Danja",
+       "Dandume",
+       "Bakori",
+       "Kankara",
+       "Malumfashi",
+       "Kafur",
+       "Musawa",
+       "Matazu",
+       "Sabuwa"
+    ],
+    "KatsinaCentral":[
+       "Katsina",
+       "Kaita",
+       "Jibia",
+       "Batsari",
+       "Safana",
+       "Dutsin-ma",
+       "Kurfi",
+       "Batagarawa",
+       "Rimi",
+       "Charanchi",
+       "DanMusa"
+    ],
+    "KebbiNorth":[
+       "Arewa",
+       "Argungu",
+       "Augie",
+       "Bagudo",
+       "Dandi",
+       "Suru",
+       "Jega"
+    ],
+    "KebbiCentral":[
+       "Aleiro",
+       "Birnin Kebbi",
+       "Bunza",
+       "Gwandu",
+       "Kalgo",
+       "Koko Besse",
+       "Maiyam"
+    ],
+    "KebbiSouth":[
+       "Fakai",
+       "Ngaski",
+       "Sakaba",
+       "Shanga",
+       "Wasgu/Danko",
+       "Yauri",
+       "Zuru"
+    ],
+    "KogiCentral":[
+       "Adavi",
+       "Ajaokuta",
+       "Ogori/Magongo",
+       "Okehi",
+       "Okene"
+    ],
+    "KogiEast":[
+       "Ankpa",
+       "Bassa",
+       "Dekina",
+       "Ibaji",
+       "Idah",
+       "Igalamela-Odolu",
+       "Ofu",
+       "Olamaboro",
+       "Omala"
+    ],
+    "KogiWest":[
+       "Ijumu",
+       "Kabba",
+       "Kogi",
+       "Lokoja",
+       "Mopamuro",
+       "Yagba East",
+       "Yagba West"
+    ],
+    "KwaraNorth":[
+       "Baruten",
+       "Edu",
+       "Patigi",
+       "Kaiama",
+       "Moro"
+    ],
+    "KwaraCentral":[
+       "Asa",
+       "Ilorin East",
+       "Ilorin West",
+       "Ilorin South",
+       "Offa"
+    ],
+    "KwaraSouth":[
+       "Ekiti",
+       "Oke-Ero",
+       "Ifelodun",
+       "Irepodun",
+       "Isin",
+       "Oyun"
+    ],
+    "LagosCentral":[
+       "Lagos Island",
+       "Lagos Mainland",
+       "Surulere",
+       "Apapa",
+       "Eti-Osa"
+    ],
+    "LagosEast":[
+       "Shamolu",
+       "Kosofe",
+       "Epe",
+       "Ibeju-Lekki",
+       "Ikorodu"
+    ],
+    "LagosWest":[
+       "Agege",
+       "Ifako-Ijaye",
+       "Alimosho",
+       "Badagry",
+       "Ojo",
+       "Ajeromi/Ifelodun",
+       "Amuwo-Odofin",
+       "Oshodi/Isolo",
+       "Ikeja",
+       "Mushin"
+    ]
+ }
+
